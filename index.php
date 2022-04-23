77c4c4b5f2acdf435970ce705f0206f62d098cac
Anisawahyuni <93024796+Anisawahyuni@users.noreply.github.com>
2022-04-23 15:55:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php
+include_once 'header.php';
+include_once 'sidebar.php';
+?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>About</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Anisa Wahyuni</a></li>
+              <li class="breadcrumb-item"><a href="#">0110121300</a></li>
+              <li class="breadcrumb-item active">Semester2</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Anisa Wahyuni</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+              <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="Anisa.css" />
+    <title>Web ANISA WAHYUNI</title>
+  </head>
+  <body>
+    <section>
+      <div class="header">
+        <h1>WELCOME I'M ANISA WAHYUNI!</h1>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="" class="A">Home</a></li>
+          <li>
+            <a href="https://www.instagram.com/p/CWlIwENhlo-/?utm_source=ig_web_copy_link" class="A"
+              >Instagram</a
+            >
+          </li>
+          <li><a href="#" class="A">More</a></li>
+        </ul>
+      </nav>
+      <fieldset>
+        <legend><b>GET TO KNOW ME</b></legend>
+        <div class="grid-container">
+          <div class="B">
+            <img src="WhatsApp Image 2022-03-25 at 21.24.06 (1).jpeg" width="300" height="325" alt="" />
+          </div>
+          <div class="text">
+            <h1>About Me</h1>
+            <p>
+              Nama saya Anisa Wahyuni, Lahir di Bogor, 09 oktober 2001, saya anak pertama dari 2 bersaudara, buah hati dari pasangan Imas&Wahyudin. sa ialah panggilan akrab dari teman-teman, saya terlahir di keluarga yang benar-benar simpel.
+              Saat berusia 7 tahun, saya mengawali pendidikan di SDN Babakan Dramaga 03, setelah lulus saya meneruskan pendidikannya di SMP Pelita Ciampea di tahun 2014. Setelah lulus dari SMP di tahun 2017 saya meneruskan pendidikan di SMK Pelita Ciampea, dan sekarang melanjutkan kuliah di STT Terpadu Nurul Fikri.
+            </p>
+            <p>Email = anisaoye2@gmail.com</p>
+            <p>Number = 0895-1802-6809</p>
+          </div>
+          <aside class="text2">
+            <h1>Categories</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur,
+              modi impedit totam aut odio omnis reprehenderit, officia vel quam,
+              voluptatibus magni necessitatibus ab amet esse architecto ullam
+              nobis sint voluptates!
+            </p>
+          </aside>
+        </div>
+        <div class="article">
+          <p style="text-align: justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            facilis recusandae libero, ipsam beatae accusamus non ullam eum
+            voluptas, repellendus laboriosam commodi laudantium sed impedit
+            autem unde! Doloribus tempore quisquam aut nisi magnam unde ducimus
+            in, perspiciatis quas totam ratione nulla placeat exercitationem
+            culpa, consectetur commodi quidem vitae eveniet est?Lorem ipsum
+            dolor, sit amet consectetur adipisicing elit. Dicta asperiores
+            accusamus numquam quo iusto quam a assumenda eum possimus nostrum
+            praesentium ipsum sunt magnam non error, delectus deleniti
+            voluptates rerum. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Nulla, est. Aut, ab esse, ipsam dolorum consequuntur et
+            architecto porro illo quasi delectus rerum quisquam accusantium
+            fugit quaerat voluptatem, tempora minus.
+          </p>
+          <p style="text-align: justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            facilis recusandae libero, ipsam beatae accusamus non ullam eum
+            voluptas, repellendus laboriosam commodi laudantium sed impedit
+            autem unde! Doloribus tempore quisquam aut nisi magnam unde ducimus
+            in, perspiciatis quas totam ratione nulla placeat exercitationem
+            culpa, consectetur commodi quidem vitae eveniet est?Lorem ipsum
+            dolor, sit amet consectetur adipisicing elit. Dicta asperiores
+            accusamus numquam quo iusto quam a assumenda eum possimus nostrum
+            praesentium ipsum sunt magnam non error, delectus deleniti
+            voluptates rerum. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Nulla, est. Aut, ab esse, ipsam dolorum consequuntur et
+            architecto porro illo quasi delectus rerum quisquam accusantium
+            fugit quaerat voluptatem, tempora minus.
+          </p>
+        </div>
+        <article class="C">
+          <video control height="300" width="350" controls>
+            <source src="VID_184461117_075730_444.mp4" />
+          </video>
+          <audio controls>
+            <source src="YURA YUNITA - Tutur Batin.mp3" />
+          </audio>
+          <div class="container">
+            <div class="container-atas">
+              <img src="WhatsApp Image 2022-03-25 at 21.24.06.jpeg" height="200" width="100" />
+              <img src="WhatsApp Image 2022-03-30 at 00.07.04.jpeg" height="200" width="100" alt="" />
+            </div>
+            <div class="container-bawah">
+              <img src="WhatsApp Image 2022-04-18 at 14.38.36.jpeg" height="200" width="100" alt="" />
+              <img src="WhatsApp Image 2022-04-23 at 14.34.27.jpeg" height="200" width="100" alt="" />
+            </div>
+          </div>
+        </article>
+      </fieldset>
+    </section>
+  </body>
+</html>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                Footer
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+<?php 
+include_once 'footer.php';
+?>
